a651bab203e7dca63078097c2e961b6a2809d4c5
Małgorzata Jańczak <m.janczak@int.pl>
2020-09-20 21:13:26 +0200

    add routing

--- app/Main.js
+++ app/Main.js
@@ -1,16 +1,36 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 
-function Example() {
+// components
+
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+
+function Main() {
   return (
-    <div>
-      <h1>hello</h1>
-      <p>aaaa</p>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about-us">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
-ReactDOM.render(<Example />, document.querySelector("#app"))
+ReactDOM.render(<Main />, document.querySelector("#app"))
 
 if (module.hot) {
   module.hot.accept()
